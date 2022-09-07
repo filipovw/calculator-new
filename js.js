daf6c6373c59ca316d3368ae0ae89d4e1054ad39
valovski <vladi2005123@gmail.com>
2022-09-07 12:39:27 +0300

    Add necessary styling

--- js.js
+++ js.js
@@ -1,1 +1,3 @@
-console.log("nasko");
+let previousResults = document.querySelector(".results-previous");
+let currentResults = document.querySelector(".results-new");
+let numbers = document.querySelectorAll(".num");
